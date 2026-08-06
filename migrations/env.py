d1ed7847914fa1bb50305c8d06c097126dc899ba
vguerrax <vguerrax@gmail.com>
2026-08-06 14:51:57 -0300

    db: Adjust db url

--- migrations/env.py
+++ migrations/env.py
@@ -11,7 +11,8 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", get_settings().database_url)
+database_url = get_settings().database_url
+config.set_main_option("sqlalchemy.url", database_url.replace("%", "%%"))
 target_metadata = Base.metadata
 
 
